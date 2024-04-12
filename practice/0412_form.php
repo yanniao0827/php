e87ff3f999a5e93a1f23d9c979aee02934a3f026
yanniao0827 <yanning0113niao@gmail.com>
2024-04-12 16:41:00 +0800

    表單

---
+++ practice/0412_form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="./0412_qs.php" method="get">
+        <input type="number" name="a" placeholder="a">
+
+        <br>
+        <input type="number" name="b" placeholder="b"><br>
+        <button type="submit">送出</button>
+    </form>
+</body>
+
+</html>
