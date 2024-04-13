0e156418faeff2ad446fdcfae058dcf2994a0b1c
yanniao0827 <yanning0113niao@gmail.com>
2024-04-13 12:13:16 +0800

    程式碼更新

--- practice/0412_qs.php
+++ practice/0412_qs.php
@@ -1,8 +1,5 @@
 <?php
 
-echo $_GET['a'] + $_GET['b'];
-echo "<br>";
-
-$c = isset($_GET['c']) ? intval($_GET['c']) : 0; //代表如果c有值就代入該值，沒有值就代入0
-$d = !empty($_GET['d']) ? intval($_GET['d']) : 0; //代表如果d不是空值就代入該值，沒有值就代入0
-echo $c + $d; //c跟d的值也可以在瀏覽器手動輸入
+$a = isset($_GET['a']) ? intval($_GET['a']) : 0; //代表如果c有值就代入該值，沒有值就代入0
+$b = !empty($_GET['b']) ? intval($_GET['b']) : 0; //代表如果d不是空值就代入該值，沒有值就代入0
+echo $a + $b; //c跟d的值也可以在瀏覽器手動輸入
