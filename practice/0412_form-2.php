1cfb13c48222fa820bc6b8ae5168907b4a15154b
yanniao0827 <yanning0113niao@gmail.com>
2024-04-13 08:42:52 +0800

    表單原檔

---
+++ practice/0412_form-2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="" method="get">
+        <input type="text" name="account" placeholder="帳號">
+        <br>
+        <input type="password" name="password" placeholder="密碼"><br>
+        <button type="submit">送出</button>
+
+    </form>
+
+
+</body>
+
+</html>
