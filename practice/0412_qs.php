611ad305b54116fd3155a76f5850b53a6b76d5b5
yanniao0827 <yanning0113niao@gmail.com>
2024-04-13 14:17:03 +0800

    新增sleep

--- practice/0412_qs.php
+++ practice/0412_qs.php
@@ -1,5 +1,5 @@
 <?php
-
+sleep(10);
 $a = isset($_GET['a']) ? intval($_GET['a']) : 0; //代表如果c有值就代入該值，沒有值就代入0
 $b = !empty($_GET['b']) ? intval($_GET['b']) : 0; //代表如果d不是空值就代入該值，沒有值就代入0
 echo $a + $b; //c跟d的值也可以在瀏覽器手動輸入
