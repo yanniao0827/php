d5f08ef6f2ce212a53c566f1dc7d214191171dd7
yanniao0827 <yanning0113niao@gmail.com>
2024-04-15 16:06:50 +0800

    日期操作

---
+++ practice/0415_12date.php
@@ -0,0 +1,12 @@
+<?php
+// date_default_timezone_set('Asia/Taipei');
+echo date("Y-m-d H:i:s");
+//現在時間
+printf("<h2>%s</h2>", date("Y-m-d H:i:s", time()));
+//一個月後時間
+printf("<h2>%s</h2>", date("Y-m-d H:i:s", time() + 30 * 24 * 60 * 60));
+//一個月後這天是星期幾
+printf("<h2>%s</h2>", date("D:w", time() + 30 * 24 * 60 * 60));
+//列出指定日期
+$t = strtotime('2024-08-27');
+printf("<h2>%s</h2>", date("Y-m-d H:i:s", $t));
