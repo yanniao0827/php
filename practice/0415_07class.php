c6bd3d3ffb20dde6717d93953fe77d096a0b5072
yanniao0827 <yanning0113niao@gmail.com>
2024-04-15 22:47:40 +0800

    類別新增註解

--- practice/0415_07class.php
+++ practice/0415_07class.php
@@ -10,10 +10,10 @@ class Person
     #建構函式
     function __construct($name, $mobile)
     {
-        $this->name = $name;
+        $this->name = $name; //$this->name指將得到的變數放到$name裡面，等號左邊需要跟類型裡面的屬性相同
         $this->mobile = $mobile;
     }
 }
 
-$p1 = new Person('Leana', '0978');
+$p1 = new Person('Leana', '0978'); //用new後面接類型名稱去建立一個物件，如果類型裡有設定建構函式，這裡的()就等同於呼叫該函式的意思
 echo " $p1->name: $p1->mobile <br>";
