a9ded400c59d8147d5c0cedb41e5bd8731cff7b3
yanniao0827 <yanning0113niao@gmail.com>
2024-04-16 16:06:53 +0800

    通訊錄管理

--- address-book/list.php
+++ address-book/list.php
@@ -2,9 +2,46 @@
 
 require __DIR__ . '/../config/pdo-connect.php';
 
-$sql = "SELECT * FROM address_book LIMIT 3";
+$perPage = 20; //每頁最多要放幾筆資料
 
-$stmt = $pdo->query($sql);
-$rows = $stmt->fetchAll();
+$page = isset($_GET['page']) ? intval($_GET['page']) : 1; //如果page沒有設定，就列出第一頁資料
+if ($page < 1) {
+    header('Location: ?page=1');
+    exit;
+}
 
-echo json_encode($rows, JSON_UNESCAPED_UNICODE);
+$t_sql = "SELECT COUNT(*) FROM address_book"; //COUNT是用來數總共有多少筆資料，()內可以放隨意數字或*，結果都不會變，但不能留白
+
+$totalRows = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0]; //把count得出來的總筆數放入$totalRows
+
+$totalPages = ceil($totalRows / $perPage);
+if ($page > $totalPages) {
+    header("Location: ?page={$totalPages}");
+    exit; # 結束這支程式
+}
+
+$totalPages = ceil($totalRows / $perPage); //兩者相除結果有餘數就無條件進入，比如如果共有21筆資料就分成兩頁
+
+// SELECT * FROM `address_book` ORDER BY sid DESC LIMIT 0, 20
+// SELECT * FROM `address_book` ORDER BY sid DESC LIMIT 20, 20
+// SELECT * FROM `address_book` ORDER BY sid DESC LIMIT 40, 20
+// SELECT * FROM `address_book` ORDER BY sid DESC LIMIT 60, 20
+
+$row = [];
+if ($totalRows) {
+    # 取得分頁資料
+    $sql = sprintf(
+        "SELECT * FROM `address_book` ORDER BY sid DESC LIMIT %s, %s",
+        ($page - 1) * $perPage,
+        $perPage
+    );
+    $rows = $pdo->query($sql)->fetchAll();
+}
+
+
+echo json_encode([
+    'totalRows' => $totalRows,
+    'totalPages' => $totalPages,
+    'page' => $page,
+    'rows' => $rows,
+]);
