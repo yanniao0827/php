f3fac427239e0d5e88127a9975d473e2fcd346d1
yanniao0827 <yanning0113niao@gmail.com>
2024-04-17 19:36:13 +0800

    列出通訊簿的三筆資料

---
+++ address-book/list-practice.php
@@ -0,0 +1,8 @@
+<?php
+require __DIR__ . '/../config/pdo-connect.php';
+
+$sql = "SELECT * FROM address_book LIMIT 3";
+
+$stmt = $pdo->query($sql); //query代表做資料表的查詢
+$rows = $stmt->fetchAll(); //將3筆資料全部讀出來
+echo json_encode($rows, JSON_UNESCAPED_UNICODE);
