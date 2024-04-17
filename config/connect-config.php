dd00738681042ca9cf0c325c058f2efa4e313ade
yanniao0827 <yanning0113niao@gmail.com>
2024-04-17 18:47:42 +0800

    連接資料庫 新增註解

--- config/connect-config.php
+++ config/connect-config.php
@@ -1,7 +1,7 @@
 <?php
 
-$db_host = 'localhost';
-$db_name = 'proj57';
-$db_user = 'root';
-$db_pass = 'P@ssw0rd';
+$db_host = 'localhost';  //資料庫主機位置
+$db_name = 'proj57'; //資料庫名稱
+$db_user = 'root'; //資料庫的使用帳號
+$db_pass = 'P@ssw0rd'; //資料庫的使用密碼
 $db_port = 3306;
