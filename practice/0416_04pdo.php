641df5ec9cad56d4582597664a90cb9237abb19d
yanniao0827 <yanning0113niao@gmail.com>
2024-04-17 18:48:12 +0800

    pdo新增註解

--- practice/0416_04pdo.php
+++ practice/0416_04pdo.php
@@ -3,6 +3,8 @@
 require './../config/connect-config.php';
 # include './../config/connect-config.php';
 
+//dsn指data sourse name，用來設定要連接的資料庫
 $dsn = "mysql:host={$db_host};dbname={$db_name};port={$db_port};charset=utf8mb4";
 
+//PDO的連接語法
 $pdo = new PDO($dsn, $db_user, $db_pass);
