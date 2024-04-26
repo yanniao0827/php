fc37897cc9e1b5b443ddb49218818308c58eb9a1
yanniao0827 <yanning0113niao@gmail.com>
2024-04-26 16:35:28 +0800

    新增資料

--- address-book/add.php
+++ address-book/add.php
@@ -7,7 +7,6 @@ $pageName = 'add';
 <?php include __DIR__ . '/parts/html-head.php' ?>
 <?php include __DIR__ . '/parts/navbar.php' ?>
 <style>
-    /* 錯誤訊息設定紅字粗體 */
     form .mb-3 .form-text {
         color: red;
         font-weight: 800;
@@ -60,21 +59,39 @@ $pageName = 'add';
         </div>
     </div>
 </div>
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">新增成功</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="alert alert-success" role="alert">
+                    資料新增成功
+                </div>
+            </div>
+            <div class="modal-footer">
+
+                <button type="button" class="btn btn-primary" onclick="location.href='list.php'">到列表頁</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">繼續新增</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php include __DIR__ . '/parts/scripts.php' ?>
 <script>
-    // 需要驗證姓名及信箱
     const nameField = document.form1.name;
     const emailField = document.form1.email;
-    // 信箱的驗證函式
+
     function validateEmail(email) {
         const re =
             /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(email);
     }
-
-
-    // TODO: 欄位資料檢查
 
     const sendData = e => {
         e.preventDefault(); // 不要讓 form1 以傳統的方式送出
@@ -90,6 +107,7 @@ $pageName = 'add';
             isPass = false;
             nameField.style.border = '1px solid red';
             nameField.nextElementSibling.innerText = '請填寫正確的姓名';
+
         }
         if (!validateEmail(emailField.value)) {
             isPass = false;
@@ -108,13 +126,16 @@ $pageName = 'add';
             }).then(r => r.json())
                 .then(data => {
                     console.log(data);
+                    if (data.success) {
+                        myModal.show();
+                    } else {
+                    }
                 })
                 .catch(ex => console.log(ex))
         }
-
-
-
-
     };
+
+    const myModal = new bootstrap.Modal('#staticBackdrop')
+
 </script>
 <?php include __DIR__ . '/parts/html-foot.php' ?>
