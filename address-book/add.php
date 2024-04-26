74921ee091f355119d2b2b5cdcf5d53580e47709
yanniao0827 <yanning0113niao@gmail.com>
2024-04-26 15:29:21 +0800

    驗證姓名、email

---
+++ address-book/add.php
@@ -0,0 +1,120 @@
+<?php
+
+$title = "新增通訊錄";
+$pageName = 'add';
+
+?>
+<?php include __DIR__ . '/parts/html-head.php' ?>
+<?php include __DIR__ . '/parts/navbar.php' ?>
+<style>
+    /* 錯誤訊息設定紅字粗體 */
+    form .mb-3 .form-text {
+        color: red;
+        font-weight: 800;
+    }
+</style>
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <div class="card">
+
+                <div class="card-body">
+                    <h5 class="card-title">新增資料</h5>
+                    <form name="form1" onsubmit="sendData(event)">
+                        <div class="mb-3">
+                            <label for="name" class="form-label">姓名</label>
+                            <input type="text" class="form-control" id="name" name="name">
+                            <div class="form-text"></div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="text" class="form-control" id="email" name="email">
+                            <div class="form-text"></div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="mobile" class="form-label">手機</label>
+                            <input type="text" class="form-control" id="mobile" name="mobile">
+                            <div class="form-text"></div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="birthday" class="form-label">生日</label>
+                            <input type="date" class="form-control" id="birthday" name="birthday">
+                            <div class="form-text"></div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="address" class="form-label">地址</label>
+
+                            <textarea class="form-control" id="address" name="address" cols="30" rows="3"></textarea>
+                            <div class="form-text"></div>
+                        </div>
+
+
+                        <button type="submit" class="btn btn-primary">新增</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include __DIR__ . '/parts/scripts.php' ?>
+<script>
+    // 需要驗證姓名及信箱
+    const nameField = document.form1.name;
+    const emailField = document.form1.email;
+    // 信箱的驗證函式
+    function validateEmail(email) {
+        const re =
+            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
+
+    // TODO: 欄位資料檢查
+
+    const sendData = e => {
+        e.preventDefault(); // 不要讓 form1 以傳統的方式送出
+
+        nameField.style.border = '1px solid #CCCCCC';
+        nameField.nextElementSibling.innerText = '';
+        emailField.style.border = '1px solid #CCCCCC';
+        emailField.nextElementSibling.innerText = '';
+        // TODO: 欄位資料檢查
+
+        let isPass = true;  // 表單有沒有通過檢查
+        if (nameField.value.length < 2) {
+            isPass = false;
+            nameField.style.border = '1px solid red';
+            nameField.nextElementSibling.innerText = '請填寫正確的姓名';
+        }
+        if (!validateEmail(emailField.value)) {
+            isPass = false;
+            emailField.style.border = '1px solid red';
+            emailField.nextElementSibling.innerText = '請填寫正確的 Email';
+        }
+
+
+        // 有通過檢查, 才要送表單
+        if (isPass) {
+            const fd = new FormData(document.form1); // 沒有外觀的表單物件
+
+            fetch('add-api.php', {
+                method: 'POST',
+                body: fd, // Content-Type: multipart/form-data
+            }).then(r => r.json())
+                .then(data => {
+                    console.log(data);
+                })
+                .catch(ex => console.log(ex))
+        }
+
+
+
+
+    };
+</script>
+<?php include __DIR__ . '/parts/html-foot.php' ?>
