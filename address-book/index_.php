be646530af6d65d6b4823ba3407edbd2e222bfa9
yanniao0827 <yanning0113niao@gmail.com>
2024-04-27 14:53:29 +0800

    首頁 新增登入狀態

--- address-book/index_.php
+++ address-book/index_.php
@@ -1,11 +1,15 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+?>
 <?php include __DIR__ . '/parts/html-head.php' ?>
 <?php include __DIR__ . '/parts/navbar.php' ?>
-
 
 <div class="container">
     <h1>Home</h1>
 </div>
-
 
 <?php include __DIR__ . '/parts/scripts.php' ?>
 <?php include __DIR__ . '/parts/html-foot.php' ?>
